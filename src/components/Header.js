86eb6f2497ef9b65cfa7f5a72801d641c45e2512
sabinteodorescu <36563618+sabinteodorescu@users.noreply.github.com>
2024-10-24 16:28:37 +0300

    react

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header class="container">
+      <div class="content">
+        <span class="blur"></span>
+        <span class="blur"></span>
+        <H1>Start <span>now </span>to become an <span>Academic Weapon</span></H1>
+        <p>
+          Take university-level courses with our AI-driven classes, covering almost every subject. Our AI models, 
+          trained on real courses from 'Lucian Blaga' University, provide accurate and relevant information. Our 
+          AI teachers tailor the courses to your knowledge and questions, delivering the information you need in 
+          the best way possible.
+        </p>
+        <a href="get started\get-started.html"><button class="btn">Get Started</button></a>
+      </div>
+      <div class="symbol"></div>
+    </header>
+  )
+}
+
+export default Header
