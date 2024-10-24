08480d73578bf6465fab9d4beac12c07b570a7a0
raulhex-7 <raul.bacila2007@gmail.com>
2024-10-24 22:32:24 +0300

    auth page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,44 +1,78 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './styles/auth.css'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./styles/auth.css";
+import {
+  faGoogle,
+  faFacebook,
+  faApple,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Auth() {
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+    console.log("Form submitted"); // Debug log
+    if (isSignUp) {
+      console.log("Signing up with", email, password); // Add logic for sign-up
+    } else {
+      console.log("Signing in with", email, password); // Add logic for sign-in
+    }
+  };
 
   return (
-    <div className="auth-body">
+    <div className={`auth-body ${isSignUp ? "sign-up-active" : ""}`}>
       <div className="auth-symbol"></div>
-      <div className="auth-container" id="container">
-        <div className="auth-form-container sign-up">
+      <div
+        className={`auth-container ${isSignUp ? "auth-active" : ""}`}
+        id="container"
+      >
+        <div
+          className={`auth-form-container ${isSignUp ? "sign-up" : "sign-in"}`}
+        >
           <form onSubmit={handleSubmit}>
-            <h1>Create Account</h1>
+            <h1>{isSignUp ? "Create Account" : "Sign In"}</h1>
             <div className="auth-social-icons">
-              <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-              <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-              <a href="#" className="icon"><i className="fa-brands fa-apple"></i></a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faGoogle} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faFacebook} />
+              </a>
+              <a href="#" className="icon">
+                <FontAwesomeIcon icon={faApple} />
+              </a>
             </div>
-            <span>or use your email for registeration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="submit" value="Sign up" />
-          </form>
-        </div>
-        <div className="auth-form-container sign-in">
-          <form onSubmit={handleSubmit}>
-            <h1>Sign In</h1>
-            <div className="auth-social-icons">
-              <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
-              <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
-              <a href="#" className="icon"><i className="fa-brands fa-apple"></i></a>
-            </div>
-            <span>or use your email password</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forget Your Password?</a>
-            <input type="submit" value="Sign in" />
+            <span>
+              {isSignUp
+                ? "or use your email for registration"
+                : "or use your email for login"}
+            </span>
+            {isSignUp && <input type="text" placeholder="Name" required />}
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            {isSignUp ? (
+              <input className="auth-btn" type="submit" value="Sign up" />
+            ) : (
+              <>
+                <a href="#">Forget Your Password?</a>
+                <input className="auth-btn" type="submit" value="Sign in" />
+              </>
+            )}
           </form>
         </div>
         <div className="auth-toggle-container">
@@ -46,18 +80,32 @@ function Auth() {
             <div className="auth-toggle-panel toggle-left">
               <h1>Welcome Back!</h1>
               <p>Enter your personal details to use all of site features</p>
-              <input type="submit" className="auth-hidden" id="login" value="Sign in" />
+              <button
+                onClick={() => setIsSignUp(false)}
+                className="auth-hidden"
+                id="login"
+              >
+                Sign in
+              </button>
             </div>
             <div className="auth-toggle-panel toggle-right">
               <h1>Hello, Friend!</h1>
-              <p>Register with your personal details to use all of site features</p>
-              <input type="submit" className="auth-hidden" id="register" value="Sign up" />
+              <p>
+                Register with your personal details to use all of site features
+              </p>
+              <button
+                onClick={() => setIsSignUp(true)}
+                className="auth-hidden"
+                id="register"
+              >
+                Sign up
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
